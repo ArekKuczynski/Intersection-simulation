d3178cf32784d4b62bb3ea98fe20d13fa30de048
Michał Malicki <nuklearbanana@gmail.com>
2024-11-25 00:06:23 +0100

    Adding get_end_points method New conditions in get_road New ideas and problems solutions in comments

--- src/roads.py
+++ src/roads.py
@@ -6,36 +6,45 @@
 #                     |    |
 #                     ^    ^
 #             road_three  road_four                 
-#elo, get_road() niech zwraca inta od 1 do 4  <--- ok
-# -----> jak coś to najpierw x potem y, ( oczywiste, ale lepiej się upewnić :v)
-
 
 class Roads():
-    def __init__(self,position = None,characteristic_points = None):
-        self.position = position
+    def __init__(self,characteristic_points = None):
         self.characteristic_points = characteristic_points
 
     # automatyczne obliczanie punktów charakterystycznych
     def get_characteristic_points(self):
-        characteristic_points = [
+        self.characteristic_points = [
             (295,250), # road_one x road_three
             (305,250), # road_one x road_four
             (295,240), # road_two x road_three
             (305,240), # road_two x road_four
             ]
-        return characteristic_points
+        return self.characteristic_points
+    
+    def get_end_points(self, number: int):
+        #### WAŻNE : Przy losowaniu ( lub tutaj ) inta, trzeba brać pod uwagę 
+        #### to, że jeśli zaczyna z np. (0,240) to end_point nie może być równy (0,250)
+        #### bo nie uwzględniamy przypadku, gdy zawraca
+        
+        if number == 1: 
+            end_point = (300,250) # road_one
+        elif number == 2:
+            end_point = (600,240) # road_two
+        elif number == 3:
+            end_point = (295,0)   # road_three
+        return end_point
     
     def get_start_points(self, number: int):
         if number == 1: 
-            start_point = (300,250) # road_one
-        if number == 2:
+            start_point = (600,250)  # road_one
+        elif number == 2:
             start_point = (0,240)  # road_two
-        if number == 3:
-            start_point = (305,0)  # road_four
-        return start_point
+        elif number == 3:
+            start_point = (305,0)  # road_three
+        return start_point:
 
-    def get_road(self,position): # sprawdza na której jest drodze na podstawie współrzędnej
-        if True:    #position not in self.characteristic_points:
+    def get_road(self,position,end_point): # sprawdza na której jest drodze na podstawie współrzędnej
+        if position not in self.characteristic_points:    
             if position[1] == 250:
                 return 1
             elif position[1] == 240:
@@ -46,7 +55,20 @@ class Roads():
                 return 4
             else:
                 return "Error"
-
+        else:
+            if end_point[1] == 250:
+                return 1
+            elif end_point[1] == 240:
+                return 2
+            elif end_point[0] == 305:
+                return 4
+            else:
+                return "Error"
+        
+        ## Będzie trzeba też uwarunkować gdy np zaczyna w (600,250) i jedzie do (295,0)
+        ## żeby dobiero na drugim punkcie charakterystycznym, który napotka zmienił kierunek
+        ## można uwarunkować to, dając start_pointy do get_road albo ręcznie warunki na punktach charakterystycznych
+        
 
     # obliczanie dalszych współrzędznych 
 
