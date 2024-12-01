4bb3401b1774f56dab3cd535cbb3b848f3aec549
Tomasz Ondruszko <tomek.ondr@gmail.com>
2024-12-01 22:46:09 +0100

    Changes in Car class: starting_engine, move, moving_forward. Expected changes in main.py

--- src/car.py
+++ src/car.py
@@ -1,37 +1,84 @@
 from data import SimData
 
+
 class Car():
     """Class for making a car object"""
-    def __init__(self, id: int, starting_position: tuple, velocity: float, length: float):
+
+    def __init__(self, id: int, starting_position: tuple, end_position: tuple, velocity: float, length: float):
         self.sim_data = SimData()
         self.id = id
         self.x, self.y = starting_position
+        self.starting_position = starting_position
+        self.end_position = end_position
         self.velocity = velocity
         self.length = length
         self.started = False
 
-    def start_engine(self):
+    def start_engine(self, road):
+        """Function which checks if car is still moving or is starting moving"""
         if not self.started:
             if len(self.sim_data.cars) == 1:
                 self.started = True
-            else:
+            if (self.x, self.y) == self.starting_position:
                 self.started = True
-                # for car in self.sim_data.cars:
-                #     while car.id <= self.id:
-                #         if car.x != self.x: # zmienić na nierówność uzwzgędniającą długości samochodu i dopisać przypadek dla 1 samochodu
-                #             self.started = True
-                #         break
+                for car in self.sim_data.cars:
+                    while car.id <= self.id:  # przy znalezieniu 1 auta, które blokuje wjazd pętla jest przerywana
+                        if road == 1:
+                            if (self.x - self.length) >= car.x and car.y == self.y:
+                                self.started = False
+                                break
+                        elif road == 2:
+                            if (self.x + self.length) <= car.x and car.y == self.y:
+                                self.started = False
+                                break
+                        elif road == 4:
+                            if (self.y + self.length) >= car.y and car.x == self.x:
+                                self.started = False
+                                break
 
-    def moving_forward(self, number: int) -> None:
+    def moving_forward(self, number: int, points: list, stop: bool) -> None:
         """Method for moving forward. Don't use in main.py"""
         if number == 1:
-            self.x -= self.velocity * self.sim_data.time_step
+            if self.x <= self.end_position[0]:
+                self.turning_off_engine()
+
+            if stop:
+                if self.x - self.velocity * self.sim_data.time_step < points[0][0]:
+                    self.x, self.y = points[0]
+                else:
+                    self.x -= self.velocity * self.sim_data.time_step
+            else:
+                self.x -= self.velocity * self.sim_data.time_step
+
         elif number == 2:
             self.x += self.velocity * self.sim_data.time_step
+            if self.x >= self.end_position[0]:
+                self.turning_off_engine()
+
+            if stop:
+                if self.x + self.velocity * self.sim_data.time_step > points[2][0]:
+                    self.x, self.y = points[2]
+                else:
+                    self.x += self.velocity * self.sim_data.time_step
+            else:
+                self.x += self.velocity * self.sim_data.time_step
+
         elif number == 3:
             self.y -= self.velocity * self.sim_data.time_step
-        else:
-            self.y += self.velocity * self.sim_data.time_step
+            if self.y <= self.end_position[1]:
+                self.turning_off_engine()
+
+        else:  # brak możliwości wyłączenia silnika, ponieważ koniec jest na przecięciu dróg
+            if stop:
+                if (self.x, self.y) == points[3]:
+                    self.y += self.velocity * self.sim_data.time_step
+                else:
+                    if self.y + self.velocity * self.sim_data.time_step > points[1][1]:
+                        self.x, self.y = points[1]
+                    else:
+                        self.y += self.velocity * self.sim_data.time_step
+            else:
+                self.y += self.velocity * self.sim_data.time_step
 
     def turning_right(self) -> None:
         """Method for turning right. Don't use in main.py"""
@@ -41,24 +88,37 @@ class Car():
         """Method for turning left. Don't use in main.py"""
         pass
 
-    def move(self, number_of_road: int) -> None:
-        """Method used in main.py"""
+    def turning_off_engine(self) -> None:
+        """Method for turning off the car's engine"""
+        self.started = False
+
+    def move(self, number_of_road: int, characteristic_points: list) -> None:
+        """Method used only in main.py"""
         print(self.started)
         if self.started:
-            self.moving_forward(number_of_road)
-            # if number_of_road == 1:
-            #     pass
-            # elif number_of_road == 2:
-            #     pass
-            # elif number_of_road == 3:
-            #     pass
-            # else:
-            #     pass
-    
+            # jeżeli musi skręcić z drogi 1 lub 2 na drogę 3
+            if self.end_position[1] != self.y:
+                self.moving_forward(
+                    number_of_road, characteristic_points, True)
+                if (self.x, self.y) == characteristic_points[0]:
+                    self.turning_left()
+                elif (self.x, self.y) == characteristic_points[2]:
+                    self.turning_right()
+
+            # jeżeli musi skręcić z drogi 4 na drogę 1 lub 2
+            elif self.end_position[0] != self.x:
+                self.moving_forward(
+                    number_of_road, characteristic_points, True)
+                if (self.x, self.y) == characteristic_points[1]:
+                    self.turning_left()
+                elif (self.x, self.y) == characteristic_points[3] and self.end_position[0] == self.x:
+                    self.turning_right()
+
+            else:
+                self.moving_forward(
+                    number_of_road, characteristic_points, False)
+
     def test(self):
         sim_data = SimData()
         sim_data.cars.append("xdCar")
         print(sim_data.cars)
-
-
-    
